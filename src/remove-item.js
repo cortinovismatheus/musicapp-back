a93c8a0268e292bc6df1c9f049ae95529ef21a06
Matheus Cortinovis <cortinovismatheus01@gmail.com>
2026-04-03 11:47:25 -0300

    novas coisas

--- src/remove-item.js
+++ src/remove-item.js
@@ -5,13 +5,13 @@ async function remove(id){
   const idToDelete = String(id);
   const dados = await readDb();
 
-  const idExistente = dados.some((item) => item.id === idToDelete);
+  const idExistente = dados.some((item) => Number(item.id) === Number(idToDelete));
 
   if(!idExistente){
     throw new Error("ID não encontrado. Por favor, escolha um ID existente para atualizar.");
   }
 
-  const novosDados = dados.filter((item) => item.id !== idToDelete);
+  const novosDados = dados.filter((item) => Number(item.id) !== Number(idToDelete));
    
   const linhas = novosDados.map((item) => {
     return `${item.id};${item.name};${item.category};${item.quantity_piece};${item.price}`; 
